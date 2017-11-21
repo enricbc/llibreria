5a72fb0e6a80d3c0ed8a6a3bab4aa9d1c86c40d1
Enric Beltran Cano <enricbeltran@iesmontsia.org>
2017-11-21 21:03:18 +0100

    Feina dia 21/11/2017

---
+++ llibreria/php/classe_autor.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ */
+class autor
+{
+  private $nom;
+  private $cognom;
+  private $pais;
+  function __construct($nom,$cognom,$pais)
+  {
+    $this->$nom=$nom;
+    $this->$cognom=$cognom;
+    $this->$pais=$pais;
+  }
+  function inserir(){
+    
+  }
+}
+
+?>
---
+++ llibreria/vista/form_autors.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Llibreria</title>
+  </head>
+  <body>
+    <form name="formulari" method="get" action="../php/alta_autor.php" >
+      <p>
+        Nom: <input type="text" name="nom" autofocus required>
+        <br>
+        <br>
+        Cognom: <input type="text" name="cognom" autofocus required>
+        <br>
+        <br>
+        Nacio-Pais: <input type="text" name="pais" autofocus required>
+        <br>
+        <br>
+        <button type="submit" name="button">Introdueix</button>
+      </p>
+    </form>
+  </body>
+</html>
