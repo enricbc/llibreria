12896b6f0167a151f6c1e125de5e4dd5cd19f035
adrianmontoro <adrianmontoro@iesmontsia.org>
2017-11-28 21:10:15 +0100

    asd

---
+++ llibreria/vista/formularigenere2.php
@@ -0,0 +1,6 @@
+<form method="post" action="esborrarGenere.php">
+<h4>Introdueix el genere a esborrar</h4>
+ID : <input type="number" name="nombre"><br> 
+<br><br>
+<input type="submit" name="valider" value="Enviar">
+</form>
