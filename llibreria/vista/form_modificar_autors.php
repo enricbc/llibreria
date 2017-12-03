518e33605586106c6c839452db0a41d1c7338654
Enric Beltran Cano <enricbeltran@iesmontsia.org>
2017-12-03 16:40:25 +0100

    Aplicant estil taula

--- llibreria/vista/form_modificar_autors.php
+++ llibreria/vista/form_modificar_autors.php
@@ -68,21 +68,33 @@
   <body style="background-color: #F5EC9A;">
     <div class="form-group container py-5">
       <form name="formulari" method="get" action="../php/modificar_autor.php" >
+        <table class="table table-inverse">
+          <thead style="background-color: #6AF574;">
+            <tr>
+              <th></th>
+              <th>Nom</th>
+              <th>Cognom</th>
+              <th>Pais/Nació</th>
+            </tr>
+          </thead>
+          <tbody style="background-color: #E2F5DD;">
         <?php
           include_once("../php/classe_autor.php");
           $result=autor::getAutors();
           if ($result->num_rows > 0){ //Si la consulta ens retorna alguna linia (Si en retorna ho posa en un array)
             while ($row = $result->fetch_array()){//Mentre que poguesim agafar elements del array
-              echo "<pre>";
-              echo "<input type='radio' name='id' value=".$row[3].">";
+              echo "<tr>";
+              echo "<th><input type='radio' name='id' value=".$row[3]."></th>";
                 for ($i=0; $i < 3; $i++) {
-                  echo $row[$i]." ";
+                  echo "<td>".$row[$i]."</td>";
                 }
-              echo "</pre>";
+              echo "</tr>";
             }
           }
          ?>
-         <button type="submit" class="btn btn-info">Selecció</button>
+       </tbody>
+     </table>
+     <button type="submit" class="btn btn-info">Selecció</button>
       </form>
     </div>
   </body>
