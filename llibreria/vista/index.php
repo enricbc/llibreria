81c067425a5fd8ac193ecaa8ec9ab1709fc4695b
Enric Beltran Cano <enricbeltran@iesmontsia.org>
2018-01-07 12:02:30 +0100

    Index noelia

--- llibreria/vista/index.php
+++ llibreria/vista/index.php
@@ -4,10 +4,25 @@
   <body style="background-color: #F5EC9A;">
     <div class="container mx-auto">
       <div class="row py-5">
-        <h1>Benvenuiti a la biblioteco</h1>
+        <h1>BIBLIOTECH</h1>
+
       </div>
     </div>
-
+    <center><img src="../images/biblioteca_alejandria.PNG";></center>
   </body>
+  
+
+<!--<center><img src="../images/biblioteca_alejandria.PNG";></center>-->
+<br>
+<br>
+<br>
+<br>
+<br>
+<center><footer style="width:100%; height:30%; float:left; position:relative;">
+  <p>E-mail: <a href>bibliotech@amposta.com</a></p>
+  <p>Website <a href>www.bibliotech.com</a></p>
+  Ubicació: <h6>C/ Cánovas nº3, Amposta, Tarragona, 43870</h6>
+</footer>
+</center
 
 </html>
